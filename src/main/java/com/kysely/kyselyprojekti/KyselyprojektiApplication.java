644f4b8ac5d76ad53291481921db055e0e41f3eb
Niemi Joni <a1704471@myy.haaga-helia.fi>
2018-10-25 09:03:22 +0300

    second commit

---
+++ src/main/java/com/kysely/kyselyprojekti/KyselyprojektiApplication.java
@@ -0,0 +1,12 @@
+package com.kysely.kyselyprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselyprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselyprojektiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kysely/kyselyprojekti/KyselyprojektiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kysely.kyselyprojekti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KyselyprojektiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
