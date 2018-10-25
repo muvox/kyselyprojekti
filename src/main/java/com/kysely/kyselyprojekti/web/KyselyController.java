4fa8143bd5bfdfe2a5a54d4b6ba2e6e759fce126
joni niemi <muvoksi@gmail.com>
2018-10-25 09:16:18 +0300

    fix

--- src/main/java/com/kysely/kyselyprojekti/web/KyselyController.java
+++ src/main/java/com/kysely/kyselyprojekti/web/KyselyController.java
@@ -1,7 +1,20 @@
 package com.kysely.kyselyprojekti.web;
 
+import com.kysely.kyselyprojekti.domain.KyselyRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by a1704471 on 25.10.2018.
  */
+@Controller
 public class KyselyController {
+    KyselyRepository repo;
+
+    @RequestMapping(value="/")
+    public String kysely(Model model){
+        model.addAttribute("kysely", repo.findAll());
+        return "booklist";
+    }
 }
